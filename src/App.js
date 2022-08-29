30858db0fc15a11542f1b9229e54eb608fb30298
qinlin891 <qinlin891@gmail.com>
2022-08-29 14:32:51 -0500

    Add initial styling

--- src/App.js
+++ src/App.js
@@ -8,15 +8,23 @@ function App() {
   const showEmployee = true;
 
   return (
-    <div className="App bg-red-300">
+    <div className="App">
       {showEmployee ? (
         <div>
           <input type="text" onChange={(e) => {
             setRole(e.target.value);
-          }}/>
-          <Employee name="Caleb" role="Intern" />
-          <Employee name="Abby" role={role} />
-          <Employee name="John" />
+          }} />
+          <div className="flex flex-wrap justify-center">
+            <Employee name="Caleb" role="Intern" img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg" />
+            <Employee name="Abby" role={role} img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"/>
+            <Employee name="John" img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"/>
+            <Employee name="Caleb" role="Intern" img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"/>
+            <Employee name="Abby" role={role} img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"/>
+            <Employee name="John" img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"/>
+            <Employee name="Caleb" role="Intern" img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"/>
+            <Employee name="Abby" role={role} img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"/>
+            <Employee name="John" img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"/>
+          </div>
         </div>
       ) : (
         <p>You cannot see the employees</p>
