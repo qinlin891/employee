a8c2ad573eae5c4f2ee0c0042e75f12a6863d6ef
qinlin891 <qinlin891@gmail.com>
2022-09-22 19:50:21 -0500

    add search word definition functionality

---
+++ src/pages/Definition.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom";
+import { v4 as uuidv4} from "uuid";
+
+export default function Definition() {
+
+    const [word, setWord] = useState();
+    let {search} = useParams();
+
+    useEffect(() => {
+        fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + search)
+        .then((response) => response.json())
+        .then((data) => {
+            setWord(data[0].meanings)
+            console.log(data[0].meanings)
+        });
+    }, []);
+
+
+    return (
+        <>
+            <h1>Here's a definition:</h1>
+            {word ? word.map((meaning) => {
+                return( 
+                <p key={uuidv4()}>
+                    {meaning.partOfSpeech}: {meaning.definitions[0].definition}
+                </p>
+                );
+            }) 
+            : null}
+        </>
+    )
+}
